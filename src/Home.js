2927d9b86b7b50abbfacbe00548063e18dca95ae
rikicop <ruperto1@protonmail.com>
2021-05-02 14:46:30 -0400

    Errors

--- src/Home.js
+++ src/Home.js
@@ -5,8 +5,8 @@ import BlogList from './BlogList';
 const Home = () => {
 
   const [blogs, setBlogs] = useState(null);
-
   const [isPending, setIsPending] = useState(true);
+  const [error,setError] = useState(null);
  /*  const handleDelete = (id) => {
     const newBlogs = blogs.filter( blog => blog.id !== id)
     setBlogs(newBlogs)
@@ -16,17 +16,27 @@ const Home = () => {
    setTimeout(()=>
    fetch('http://localhost:8000/blogs')
     .then(res => {
+     
+      if(!res.ok){
+        throw Error('No se cargaron datos')
+      }
       return res.json();
     }) 
     .then(data => {
       setBlogs(data);
       setIsPending(false);
+      setError(null)
+    })
+    .catch(err =>{
+      setIsPending(false)
+      setError(err.message)
     })
    ,1000)
   },[]);
 
   return (
       <div className="home">
+        {error && <div style={{color:'red'}}> {error} </div>}
         {isPending && <div> Cargando... </div> }
         { blogs && <BlogList blogs = {blogs} title="Todos Los Blogs"  /> } 
       </div>
