3b61779334db151e1eaa88d9004e0401f0ec1305
rikicop <ruperto1@protonmail.com>
2021-05-03 18:13:29 -0400

    custom hook

--- src/Home.js
+++ src/Home.js
@@ -1,39 +1,10 @@
-import {useState, useEffect} from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch'
 
 
 const Home = () => {
-
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error,setError] = useState(null);
- /*  const handleDelete = (id) => {
-    const newBlogs = blogs.filter( blog => blog.id !== id)
-    setBlogs(newBlogs)
-  } */
-  
-  useEffect(()=>{
-   setTimeout(()=>
-   fetch('http://localhost:8000/blogs')
-    .then(res => {
-     
-      if(!res.ok){
-        throw Error('No se cargaron datos')
-      }
-      return res.json();
-    }) 
-    .then(data => {
-      setBlogs(data);
-      setIsPending(false);
-      setError(null)
-    })
-    .catch(err =>{
-      setIsPending(false)
-      setError(err.message)
-    })
-   ,1000)
-  },[]);
-
+  const {data : blogs, isPending, error} = useFetch('http://localhost:8000/blogs')
+   
   return (
       <div className="home">
         {error && <div style={{color:'red'}}> {error} </div>}
